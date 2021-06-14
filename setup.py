5e9be048785f5903065f6c90a4b24ca6e47c4d8b
Miles Rack <milesr4@protonmail.com>
2021-06-14 11:50:03 -0400

    Add project URL

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('README.md', 'r') as f:
     LONG_DESCRIPTION = f.read()
 
-VERSION = '1.0.0' 
+VERSION = '1.0.1' 
 DESCRIPTION = 'Python package to interact with Houseparty\'s API.'
 
 setup(
@@ -14,6 +14,8 @@ setup(
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         long_description_content_type='text/markdown',
+        url='https://github.com/milesrack/houseparty',
+        license='MIT',
         packages=find_packages(),
         install_requires=['requests'],
         keywords=['python', 'houseparty', 'api', 'houseparty-app'],
